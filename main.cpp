f62769451de9746fff0a1880a7298c930039811c
amaru666 <jeffrey.orihuela@utec.edu.pe>
2018-09-11 21:54:33 -0500

    testing

--- main.cpp
+++ main.cpp
@@ -8,7 +8,14 @@ using namespace std;
 template <typename T>
 struct Dimensions {      
     int operator()(int* coordinates, int* sizes, int dimensions) {
-        // TODO
+        int suma = 0;
+        int producto = 1;
+        for(int i = 0; i < dimensions; i++){
+            if(i>0) producto *= sizes[i-1];
+
+            suma+=sizes[i]*producto;
+        }
+        return suma;
     }
 };
 
